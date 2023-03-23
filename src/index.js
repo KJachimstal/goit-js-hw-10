4e3bbe52895cba512b376ca0bab71599e0ec7af1
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-23 17:45:22 +0100

    Link HTML with JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const search = document.querySelector('#search-box');
+const countrysList = document.querySelector('.country-list');
+const countryDetails = document.querySelector('.country-info');
